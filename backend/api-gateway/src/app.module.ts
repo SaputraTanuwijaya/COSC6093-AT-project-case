469b8795ac05aba1ea2ee383f07ad3d93c30fa2d
SaputraTanuwijaya <saputratanuwijaya@gmail.com>
2025-10-27 17:19:47 +0700

    Configure microservice communication and injectable PrismaService

--- backend/api-gateway/src/app.module.ts
+++ backend/api-gateway/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [
+    PrismaModule,
+    ClientsModule.register([
+      {
+        name: 'ECOMMERCE_SERVICE',
+        transport: Transport.TCP,
+        options: {
+          host: 'localhost',
+          port: 3001,
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
